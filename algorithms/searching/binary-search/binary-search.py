0803441d9896a5ead507448602c35aa284e0a6be
Gabriel Alves <gabrielalves6002@gmail.com>
2025-06-21 11:35:24 -0300

    chore: organize folders

---
+++ algorithms/searching/binary-search/binary-search.py
@@ -0,0 +1,40 @@
+import json
+
+NAMES_LIST = '../../../lists/ordered-names-128.json'
+
+def binarySearch(list, item):
+  low = 0
+  high = len(list) - 1
+  steps = 0
+
+  while low <= high:
+    middle = (low + high) // 2 
+    guess = list[middle]
+    # print(middle)
+
+    if guess == item:
+      steps = steps + 1
+      print(f"Passos dados: {steps}")
+      return middle
+    if guess > item:
+      steps = steps + 1
+      high = middle - 1
+    else:
+      steps = steps + 1
+      low = middle + 1
+
+  print(f"Passos dados: {steps}")
+  return -1
+
+with open(NAMES_LIST, 'r', encoding='utf-8') as file:
+  data = json.load(file)
+  names = data['list']
+
+target = input('Nome: ')
+index = binarySearch(names, target)
+
+if index == -1:
+  print('Nome não listado.')
+  exit()
+
+print(f"O nome {names[index]} foi encontrado no index {index}!")
